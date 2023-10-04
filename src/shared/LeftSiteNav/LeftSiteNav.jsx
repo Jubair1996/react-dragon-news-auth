b036f9aa62f8aeb8145177a20205e8ccf3437094
Jubari1996 <abujubari1996@gmail.com>
2023-10-04 19:58:26 +0600

    Left site nav news category  section added

--- src/shared/LeftSiteNav/LeftSiteNav.jsx
+++ src/shared/LeftSiteNav/LeftSiteNav.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftSiteNav = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() =>{
+        fetch("categories.json")
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    },[])
     return (
         <div>
-            <h1 className="text-5xl">Lift Site Nav</h1>
+            <h1 className="text-xl font-semibold text-[#403F3F]">All Categroy</h1>
+            <h3 className="text-xl font-semibold text-[#403F3F] bg-[#E7E7E7] w-full text-center my-4 py-2 rounded">National News </h3>
+
+            <div className="space-y-3 ms-16">
+                {
+                    categories.map(category =><NavLink to={`category / ${category.id}`} className="block text-[#9F9F9F] text-xl font-medium" key={category.id}>{category.name}</NavLink>)
+                }
+            </div>
         </div>
     );
 };
