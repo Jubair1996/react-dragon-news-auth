5bdd82b6acec02de07a018bc69af7dfeefb80d8d
Jubari1996 <abujubari1996@gmail.com>
2023-10-04 15:19:52 +0600

    Navbar section added

--- src/Pages/Home/BreakingNews.jsx
+++ src/Pages/Home/BreakingNews.jsx
@@ -1,13 +1,16 @@
-
+import Marquee from "react-fast-marquee";
 const BreakingNews = () => {
-    return (
-        <div>
-          <button className="btn btn-secondary">Latest</button> 
-          <div>
-                
-         </div> 
-        </div>
-    );
+  return (
+    <div className="flex gap-6 bg-[#F3F3F3] justify-center items-center px-4 mb-4 py-2 rounded">
+      <button className="bg-[#D72050] px-6 py-2 text-white rounded uppercase font-semibold ">Latest</button>
+      <div>
+        <Marquee pauseOnHover={true} speed={100} >
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+    </div>
+  );
 };
 
 export default BreakingNews;
