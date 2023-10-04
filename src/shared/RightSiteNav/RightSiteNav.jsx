d4ad99a7e6fb647b627a939b02a5b94ccf8f3612
Jubari1996 <abujubari1996@gmail.com>
2023-10-04 17:36:51 +0600

    Right site nav  section added

--- src/shared/RightSiteNav/RightSiteNav.jsx
+++ src/shared/RightSiteNav/RightSiteNav.jsx
@@ -1,8 +1,9 @@
-import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-import qZone1 from "../../assets/qZone1.png"
-import qZone2 from "../../assets/qZone2.png"
-import qZone3 from "../../assets/qZone3.png"
+import bg from "../../assets/bg.png";
+import qZone1 from "../../assets/qZone1.png";
+import qZone2 from "../../assets/qZone2.png";
+import qZone3 from "../../assets/qZone3.png";
 const RightSiteNav = () => {
   return (
     <div className="p-4">
@@ -16,29 +17,51 @@ const RightSiteNav = () => {
         Login with github
       </button>
 
-
       <div>
-      <h1 className="text-2xl font-semibold mt-4 mb-2">Login With</h1>
-      <a href="" className="flex gap-4 items-center  border px-2 py-3 rounded-t-lg">
-        <FaFacebook className="text-2xl bg-[#F3F3F3] rounded-full p-1 text-blue-400"></FaFacebook>
-        Facebook
-      </a>
-      <a href="" className="flex gap-4 items-center border-x px-2 py-3">
-        <FaTwitter className="text-2xl text-blue-300 bg-[#F3F3F3] rounded-full p-1"></FaTwitter>
-        Twitter
-      </a>
-      <a href="" className="flex gap-4 items-center mb-2 border px-2 py-3 rounded-b-lg">
-        <FaInstagram className="text-2xl text-pink-300 bg-[#F3F3F3] rounded-full p-1"></FaInstagram>
-        Instagram
-      </a>
+        <h1 className="text-2xl font-semibold mt-4 mb-2">Login With</h1>
+        <a
+          href=""
+          className="flex gap-4 items-center  border px-2 py-3 rounded-t-lg"
+        >
+          <FaFacebook className="text-2xl bg-[#F3F3F3] rounded-full p-1 text-blue-400"></FaFacebook>
+          Facebook
+        </a>
+        <a href="" className="flex gap-4 items-center border-x px-2 py-3">
+          <FaTwitter className="text-2xl text-blue-300 bg-[#F3F3F3] rounded-full p-1"></FaTwitter>
+          Twitter
+        </a>
+        <a
+          href=""
+          className="flex gap-4 items-center mb-2 border px-2 py-3 rounded-b-lg"
+        >
+          <FaInstagram className="text-2xl text-pink-300 bg-[#F3F3F3] rounded-full p-1"></FaInstagram>
+          Instagram
+        </a>
       </div>
 
+      <div className="bg-[#F3F3F3] rounded p-4">
+        <h1 className="text-2xl font-semibold mt-4 mb-2">Q Zone</h1>
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
+      </div>
 
-      <div className='bg-[#F3F3F3] p-4'>
-      <h1 className="text-2xl font-semibold mt-4 mb-2">Q Zone</h1>
-      <img src={qZone1} alt="" />
-      <img src={qZone2} alt="" />
-      <img src={qZone3} alt="" />
+      <div>
+        <div className="card   image-full mt-4">
+          <figure>
+            <img
+              src={bg}
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-center font-bold text-2xl">Create an Amazing Newspaper!</h2>
+            <p className="text-center">Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+            <div className="card-actions justify-center">
+              <button className="bg-[#D72050] px-6 py-2 text-white rounded uppercase font-semibold ">Learn More</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
